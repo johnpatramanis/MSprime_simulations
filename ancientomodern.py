f6b1afd7b0a679ca12ef1aa83fd079483a314caf
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2018-11-19 13:23:20 +0200

    Update ancientomodern.py

--- ancientomodern.py
+++ ancientomodern.py
@@ -1,49 +1,46 @@
 import msprime
 import numpy as np
 import math
+import random
+
+
+N_OUTGROUP=10000
+N_ANCIENT=1000
+N0_MODERN=1000
 
 
 
-N_AF=10000
-N_OUT=1000
-N_Ancient=1000
 
 
 
-
-
-
-r_AF=0.00
-r_Ancient=0.001
-r_Modern=0.001
+r_OUT=0.00
+r_ANCIENT=0
+r_MODERN=0.001
 
 
 generation_time = 23
 
 
-T_split_AFRICA_OUTOFAFRICA= 80000/generation_time
-T_split_Ancient_Modern= 4500/generation_time
+T_split_OUTGROUP= 10000/generation_time
+T_split_Ancient_Modern= 2500/generation_time
 T_Growth_Begins=700/generation_time
 T_Serious_Growth=100/generation_time
 
 
-T_archaic_sampling=4000/generation_time
+T_archaic_sampling=2500/generation_time
 
 
 
 ########################## Modern population size with 2 different growth rates ??
-N_Modern=N_OUT / math.exp(-r_Modern * T_split_AFRICA_OUTOFAFRICA)
+N_MODERN=N0_MODERN / math.exp(-r_MODERN * T_split_Ancient_Modern)
 
 
 
 
 population_configurations = [
-    msprime.PopulationConfiguration(initial_size=N_DENI),
-    msprime.PopulationConfiguration(initial_size=N_NEAD1),
-    msprime.PopulationConfiguration(initial_size=N_NEAD2),
-    msprime.PopulationConfiguration(initial_size=N_AFRICA),
-    msprime.PopulationConfiguration(initial_size=N_EU),
-    msprime.PopulationConfiguration(initial_size=N_ASIA)
+    msprime.PopulationConfiguration(initial_size=N_OUTGROUP),
+    msprime.PopulationConfiguration(initial_size=N_ANCIENT),
+    msprime.PopulationConfiguration(initial_size=N_MODERN)
 ]
 
 #migrations
@@ -54,72 +51,39 @@ m_=0.00001
 
 migration_matrix = [
 ####DE,NE1,NE2,AF,EU,AS##
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0.01,0.001],
-    [0,0,0,0.01,0,0.01],
-    [0,0,0,0.001,0.01,0]
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
 ]
 
-samples=[msprime.Sample(0,T_archaic_sampling)]*6 + [msprime.Sample(1,T_archaic_sampling)]*6 + [msprime.Sample(2,T_archaic_sampling)] *6 + [msprime.Sample(3,0)] *6 + [msprime.Sample(4,0)] *6 + [msprime.Sample(5,0)] *6
+samples=[msprime.Sample(0,0)]*100 + [msprime.Sample(1,T_archaic_sampling)]*100 + [msprime.Sample(2,0)]*100 
 
 
 demographic_events = [
 ######################################################################################
-#Split EU-AS ,migrations set to 0, growth set to 0, population 4 size set to out_of_africa
+#
 
-msprime.MassMigration(time=T_split_EU_ASIA,source=5,destination=4,proportion = 1.0),
+msprime.MassMigration(time=T_split_Ancient_Modern,source=2,destination=1,proportion = 1.0),
+msprime.PopulationParametersChange(time =T_split_Ancient_Modern , initial_size = N0_MODERN , growth_rate = 0, population_id = 1),
 
-msprime.PopulationParametersChange(time =T_split_EU_ASIA , initial_size = N_OUT_OF_AFRICA , growth_rate = 0, population_id = 4),
-msprime.PopulationParametersChange(time =T_split_EU_ASIA , growth_rate = 0, population_id = 5),
-
-msprime.MigrationRateChange(time=T_split_EU_ASIA,rate=0),
+#msprime.MigrationRateChange(time=T_split_EU_ASIA,rate=0),
 
 ########################################################################################
+########################################################################################
+#
 
-#Introgration
+msprime.MassMigration(time=T_split_OUTGROUP,source=0,destination=1,proportion = 1.0),
 
-msprime.MigrationRateChange(time=T_introgration1,rate=0, matrix_index=(1, 4)),
-msprime.MigrationRateChange(time=T_introgration1,rate=0.02, matrix_index=(4, 1)),
-
-#End of Introgration
-
-msprime.MigrationRateChange(time=T_introgration1+5,rate=0, matrix_index=(1, 4)),
-msprime.MigrationRateChange(time=T_introgration1+5,rate=0, matrix_index=(4, 1)),
-
-
-
+#msprime.PopulationParametersChange(time =T_split_AFRICA_OUTOFAFRICA , initial_size = N_OG_SAPIENS , growth_rate = 0, population_id = 4)
 
 ########################################################################################
-#Split Africa-Eurasian, population 3 size set to homo sapiens
-
-msprime.MassMigration(time=T_split_AFRICA_OUTOFAFRICA,source=4,destination=3,proportion = 1.0),
-
-msprime.PopulationParametersChange(time =T_split_AFRICA_OUTOFAFRICA , initial_size = N_OG_SAPIENS , growth_rate = 0, population_id = 4),
-
-########################################################################################
-#Split of 2 neanderthal lineages
-
-msprime.MassMigration(time=T_split_NEAD1_NEAD2,source=1,destination=2,proportion = 1.0),
-########################################################################################
-#Split of Neanderthal and Denisova, population 0 size set to Neanderthal-Denisova
-
-msprime.MassMigration(time=T_split_NEAD_DENI,source=2,destination=0,proportion = 1.0),
-msprime.PopulationParametersChange(time =T_split_NEAD_DENI , initial_size = N_OG_NEAD_DENI , population_id = 0),
-
-########################################################################################
-#Split of sapiens with archaic
-
-msprime.MassMigration(time=T_split_SAPIENS_NEAD_DENI,source=3,destination=0,proportion = 1.0)
-
 ]
 
 chrom=1
 
 #recomb_map=msprime.RecombinationMap.read_hapmap('genetic_map_GRCh37_chr{}.txt'.format(chrom))
 
-n_replicates=1000
+n_replicates=1
 random_seed=random.randint(0,100000)
 
 dd = msprime.simulate(samples=samples,
