119c672afc47bc13acb0ff5594d6b0a8cbba20b2
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2018-12-17 16:23:22 +0200

    Update Simulation_fstat.py

--- Simulation_fstat.py
+++ Simulation_fstat.py
@@ -2,8 +2,8 @@ import msprime
 import numpy as np
 import math
 import os
+import time
 import re
-
 
 
 N_OG=1000
@@ -17,8 +17,8 @@ r_B=0.000
 
 generation_time = 25
 
-T_split_OUT_AB=25000/generation_time
-T_split_AB=12500/generation_time
+T_split_OUT_AB=100000/generation_time
+T_split_AB=50000/generation_time
 
 
 
@@ -35,12 +35,12 @@ population_configurations = [
 ]
 
 #migration of AB to OUT
-m_OUT_AB=0.00000
+m_OUT_AB=0.001
 
 migration_matrix = [
-    [0,0.00000,0.000000],
-    [0.00000,0,0.00000],
-    [0.000000,0.00000,0]]
+    [0,0.0001,0.0001],
+    [0.0001,0,0.0001],
+    [0.0001,0.0001,0]]
 
 samples=[msprime.Sample(0,0)]*100 + [msprime.Sample(1,0)]*100 + [msprime.Sample(2,0)] *100
 
@@ -58,147 +58,179 @@ demographic_events = [
     msprime.PopulationParametersChange(time=T_split_OUT_AB, initial_size=N_OUT, population_id=0)
 ]
 
-dd = msprime.simulate(samples=samples,
-    population_configurations=population_configurations,
-    migration_matrix=migration_matrix,mutation_rate=0.01,
-    demographic_events=demographic_events,length=1.0, recombination_rate=2e-8,num_replicates=22)
 
-j=1
-variants=[]
-for i in dd:
-    
-    for var in i.variants():
-        variants.append(var.position)
-    wow=open('mynewvcf{}.vcf'.format(j),'w')
-    i.write_vcf(wow,2,str(j))
-    wow.close()
-#    print(dd.samples())
-    population_labels= ["africa"]*50 + ["asia"]*50 + ["europe"]*50
-    d=0
-    newlabels=[]
-    for i in range(0,len(population_labels)):
-        newlabels.append(population_labels[i]+str(d))
-        d+=1
-        if i==len(population_labels)-2:
+reps=1000
+totalf3=[]
+for REPS in range(0,reps):
+    dd = msprime.simulate(samples=samples,
+        population_configurations=population_configurations,
+        migration_matrix=migration_matrix,mutation_rate=0.01,
+        demographic_events=demographic_events,length=1.0, recombination_rate=2e-8,num_replicates=22)
+
+    j=1
+    variants=[]
+    for i in dd:
+        
+        for var in i.variants():
+            variants.append(var.position)
+        wow=open('mynewvcf{}.vcf'.format(j),'w')
+        i.write_vcf(wow,2,str(j))
+        wow.close()
+    #    print(dd.samples())
+        population_labels= ["africa"]*50 + ["asia"]*50 + ["europe"]*50
+        d=0
+        newlabels=[]
+        for i in range(0,len(population_labels)):
             newlabels.append(population_labels[i]+str(d))
-            break
-        if population_labels[i]!=population_labels[i+1]:
-            d=0
-    population_labels=newlabels
-    wow=open('mynewvcf{}.vcf'.format(j))
-    wowzers=open('myvcf{}.vcf'.format(j),'w')
-    for line in wow:
-        line=line.strip().split()
-        if line[0]=='#CHROM':
-            line[9:]=population_labels
-        wowzers.write("\t".join(line))
-        wowzers.write("\n")
-    wow.close()
-    wowzers.close()
-    j+=1
+            d+=1
+            if i==len(population_labels)-2:
+                newlabels.append(population_labels[i]+str(d))
+                break
+            if population_labels[i]!=population_labels[i+1]:
+                d=0
+        population_labels=newlabels
+        wow=open('mynewvcf{}.vcf'.format(j))
+        wowzers=open('myvcf{}.vcf'.format(j),'w')
+        for line in wow:
+            line=line.strip().split()
+            if line[0]=='#CHROM':
+                line[9:]=population_labels
+            wowzers.write("\t".join(line))
+            wowzers.write("\n")
+        wow.close()
+        wowzers.close()
+        j+=1
 
-os.system('rm mynewvcf*.vcf')
-os.system('bcftools concat -o total_chroms.vcf myvcf*.vcf')
-os.system('rm myvcf*.vcf')
-
-
-
-
-VCF=open('total_chroms.vcf','r')
-newVCF=open('newtotal_chroms.vcf','w')
-
-snpcount=0
-for line in VCF:
-    if line[0]!='#' and snpcount<len(variants):
-        line=line.strip().split()
-        line[2]='rs{}'.format(snpcount)
-        line[1]=str(variants[snpcount])
-        line.append('\n')
-        line='\t'.join(line)
-        snpcount+=1
-    newVCF.write(line)
-
-VCF.close
-newVCF.close
-
-os.system('mv newtotal_chroms.vcf total_chroms.vcf')
-os.system('plink --vcf total_chroms.vcf --make-bed --out simulation')
-
-####################################### 3 Pop Test ######################################################################################
-parfile=open('parfile.txt','w')
-
-parfile.write('genotypename:    simulation.bed\n')
-parfile.write('snpname:         simulation.bim\n')
-parfile.write('indivname:       simulation.fam\n')
-parfile.write('outputformat:   PACKEDANCESTRYMAP\n')
-parfile.write('genotypeoutname: simulation.geno\n')
-parfile.write('snpoutname:      simulation.snp\n')
-parfile.write('indivoutname:    simulation.ind\n')
-parfile.write('pordercheck: NO')
-
-parfile.close()
+    os.system('rm mynewvcf*.vcf')
+    os.system('bcftools concat -o total_chroms.vcf myvcf*.vcf')
+    os.system('rm myvcf*.vcf')
 
 
-os.system('convertf -p parfile.txt')
 
-IND=open('simulation.ind','r')
-newIND=open('newsimulation.ind','w')
 
-for line in IND:
-    line=line.strip().split()
-    label=re.search(r'([a-z]+)([0-9]+):[a-z]+[0-9]+',line[0])
-    pop=label.group(1)
-    number=label.group(0)
-    line[0]=str(number)
-    line[2]=str(pop)
-    newIND.write('\t'.join(line))
-    newIND.write('\n')
+    VCF=open('total_chroms.vcf','r')
+    newVCF=open('newtotal_chroms.vcf','w')
+
+    snpcount=0
+    for line in VCF:
+        if line[0]!='#' and snpcount<len(variants):
+            line=line.strip().split()
+            line[2]='rs{}'.format(snpcount)
+            line[1]=str(variants[snpcount])
+            line.append('\n')
+            line='\t'.join(line)
+            snpcount+=1
+        newVCF.write(line)
+
+    VCF.close
+    newVCF.close
+
+    os.system('mv newtotal_chroms.vcf total_chroms.vcf')
+
+    os.system('plink --vcf total_chroms.vcf --make-bed --out simulation')
+
+
+
     
-IND.close
-newIND.close()
+
+    import os.path
+    if os.path.isfile('simulation.bed'):
+        simulationfile='simulation'
+    else:
+        simulationfile='simulation-temporary'
     
-os.system('mv newsimulation.ind simulation.ind')
+    os.system('plink --bfile {} --pca 10 --out pcaofsimulation'.format(simulationfile))
+    
+    
+    ####################################### 3 Pop Test ######################################################################################
+    parfile=open('parfile.txt','w')
 
-Pop3=open('qp3Poplist','w')
-Pop3.write('africa europe asia')
-Pop3.close()
+    parfile.write('genotypename:    {}.bed\n'.format(simulationfile))
+    parfile.write('snpname:         {}.bim\n'.format(simulationfile))
+    parfile.write('indivname:       {}.fam\n'.format(simulationfile))
+    parfile.write('outputformat:   PACKEDANCESTRYMAP\n')
+    parfile.write('genotypeoutname: simulation.geno\n')
+    parfile.write('snpoutname:      simulation.snp\n')
+    parfile.write('indivoutname:    simulation.ind\n')
+    parfile.write('pordercheck: NO')
 
-Parfilepop=open('3popparfile','w')
-Parfilepop.write('SSS: allmap\n')
-Parfilepop.write('indivname:   simulation.ind\n')
-Parfilepop.write('snpname:     simulation.snp\n')
-Parfilepop.write('genotypename: simulation.geno\n')
-Parfilepop.write('popfilename: qp3Poplist\n')
-
-Parfilepop.close()
-
-
-#SNP=open('simulation.snp','r')
-#newSNP=open('newsimulation.snp','w')
-#snpcounter=0
-#for line in SNP:
-#    if snpcounter<len(variants):
-#        line=line.strip().split()
-#        line[0]='rs{}'.format(snpcounter)
-#        line[2]=str(variants[snpcounter])
-#        line.append('\n')
-#        line='\t'.join(line)
-#        snpcounter+=1
-#        newSNP.write(line)
+    parfile.close()
 
 
+    os.system('convertf -p parfile.txt')
+
+    IND=open('simulation.ind','r')
+    newIND=open('newsimulation.ind','w')
+
+    for line in IND:
+        line=line.strip().split()
+        label=re.search(r'([a-z]+)([0-9]+):[a-z]+[0-9]+',line[0])
+        pop=label.group(1)
+        number=label.group(0)
+        line[0]=str(number)
+        line[2]=str(pop)
+        newIND.write('\t'.join(line))
+        newIND.write('\n')
+        
+    IND.close
+    newIND.close()
+        
+    os.system('mv newsimulation.ind simulation.ind')
+
+    Pop3=open('qp3Poplist','w')
+    Pop3.write('africa europe asia')
+    Pop3.close()
+
+    Parfilepop=open('3popparfile','w')
+    Parfilepop.write('SSS: allmap\n')
+    Parfilepop.write('indivname:   simulation.ind\n')
+    Parfilepop.write('snpname:     simulation.snp\n')
+    Parfilepop.write('genotypename: simulation.geno\n')
+    Parfilepop.write('popfilename: qp3Poplist\n')
+
+    Parfilepop.close()
 
 
-#SNP.close
-#newSNP.close
+    #SNP=open('simulation.snp','r')
+    #newSNP=open('newsimulation.snp','w')
+    #snpcounter=0
+    #for line in SNP:
+    #    if snpcounter<len(variants):
+    #        line=line.strip().split()
+    #        line[0]='rs{}'.format(snpcounter)
+    #        line[2]=str(variants[snpcounter])
+    #        line.append('\n')
+    #        line='\t'.join(line)
+    #        snpcounter+=1
+    #        newSNP.write(line)
 
-#os.system('mv newsimulation.snp simulation.snp')
-os.system('ls')
 
 
 
-os.system('qp3Pop -p 3popparfile')
+    #SNP.close
+    #newSNP.close
 
+    #os.system('mv newsimulation.snp simulation.snp')
+    os.system('ls')
+
+
+
+    os.system('qp3Pop -p 3popparfile >f3stat')
+    f3file=open('f3stat','r')
+    for line in f3file:
+        line=line.strip().split()
+        #print(line)
+        if line[0]=='result:':
+            totalf3.append(float(line[4]))
+    f3file.close()
+    
+    
+    
+    
+    os.system('rm simulation.*')
+    os.system('rm simulation-temporary.*')
+
+print(np.mean(totalf3))
 
 
 ###############################################################################################################################################
